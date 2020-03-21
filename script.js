64149d210a5b577b724a47a505d037101ce93e84
inkhl369 <60577224+inkhl369@users.noreply.github.com>
2020-03-21 23:54:51 +0300

    init:add newfile

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 const MENU = document.getElementById('menu');
-const MENU_ITEM = MENU.querySelectorAll('li');
+const MENU_N = MENU.querySelectorAll('li');
 MENU.addEventListener('click', (evt) => {
-    MENU_ITEM.forEach(el => el.querySelector('a').classList.remove('active'));
+    MENU_N.forEach(el => el.querySelector('a').classList.remove('active'));
     evt.target.classList.add('active');
 })
 
 
 
 
-const BUTTONS_BOX = document.getElementById('portfolio-button');
-const BUTTONS = BUTTONS_BOX.querySelectorAll('button');
+const BUTTON = document.getElementById('portfolio-button');
+const BUTTONS = BUTTON.querySelectorAll('button');
 
 
-BUTTONS_BOX.addEventListener('click', (evt) => {
+BUTTON.addEventListener('click', (evt) => {
     BUTTONS.forEach(el => el.classList.remove('active'));
 
     const IMAGES_BOX = document.getElementById('picture');
     const IMAGES = IMAGES_BOX.querySelectorAll('img');
     const TAB1 = [];
-    for (let i = 0; i < IMAGES.length; i++) {
-        let currentImage;
+    for (var i = 0; i < IMAGES.length; i++) {
+        var currentImage;
         if (i === IMAGES.length - 1) {
             currentImage = IMAGES[0];
         } else {
@@ -28,8 +28,8 @@ BUTTONS_BOX.addEventListener('click', (evt) => {
         }
         TAB1.push(currentImage);
     }
-    IMAGES_BOX.innerHTML = '';
-    for (let i = 0; i < IMAGES.length; i++) {
+    IMAGES_BOX.insideHTML = '';
+    for (var i = 0; i < IMAGES.length; i++) {
         IMAGES_BOX.append(TAB1[i]);
     }
 
@@ -43,7 +43,7 @@ const left_arrow = document.querySelector(".left-arrow");
 const right_arrow = document.querySelector(".right-arrow");
 const slides = document.getElementsByClassName("mobile_phone_slides");
 const mobile_phone = document.querySelector(".mobile_phone");
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 left_arrow.addEventListener('click', (e) => {
   plusSlides(-1);
@@ -63,30 +63,30 @@ function showSlides(n) {
     if (n < 1) {
       slideIndex = slides.length;
     }
-    for (let i = 0; i < slides.length; i++) {
+    for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "";
-    if (slideIndex - 1 == 1){
-        mobile_phone.classList.add('blue');
+    if (slideIndex - 1 !== 1){
+        mobile_phone.classList.remove('blue');
     }
     else {
-        mobile_phone.classList.remove('blue');
+        mobile_phone.classList.add('blue');
     }
 }
 
 
 
 
-let verticalIphoneButton = document.querySelector('.iphone-vertical_button');
-let blackScreenVertical = document.querySelector('.mobile_phone_black-screen-vertical');
+var verticalIphoneButton = document.querySelector('.iphone-vertical_button');
+var blackScreenVertical = document.querySelector('.mobile_phone_black-screen-vertical');
 
 verticalIphoneButton.addEventListener('click', (event) => {
 blackScreenVertical.classList.toggle('screen-on');
 });
 
-let horizontalIphoneButton = document.querySelector('.iphone-horizontal_button');
-let blackScreenHorizontal = document.querySelector('.mobile_phone_black-screen-horizontal');
+var horizontalIphoneButton = document.querySelector('.iphone-horizontal_button');
+var blackScreenHorizontal = document.querySelector('.mobile_phone_black-screen-horizontal');
 
 horizontalIphoneButton.addEventListener('click', (event) => {
 blackScreenHorizontal.classList.toggle('screen-on');
@@ -96,22 +96,22 @@ blackScreenHorizontal.classList.toggle('screen-on');
 
 document.querySelector('.form').addEventListener('submit', (event) => {
     event.preventDefault();
-    let subject = document.querySelector('.subject').value;
-    subject = subject === '' ? "Отсутствие темы" : "Тема: " + subject;
+    var str = document.querySelector('.subject').value;
+    str = str === '' ? "Отсутствие темы" : "Тема: " +  str;
     if (document.querySelector('.name').checkValidity() && document.querySelector('.email').checkValidity()){
       document.querySelector('.modal_subject').innerHTML = '';
-      document.querySelector('.modal_subject').innerHTML = subject;
-      let description = document.querySelector('.form_textarea').value;
-      description = description === '' ? "Отсутствие описания проекта" : "Описание проекта: " + description;
+      document.querySelector('.modal_subject').innerHTML = str;
+      let content = document.querySelector('.form_textarea').value;
+      content = content === '' ? "Отсутствие описания проекта" : "Описание проекта: " + content;
       document.querySelector('.modal_description').innerHTML = '';
-      document.querySelector('.modal_description').innerHTML = description;
+      document.querySelector('.modal_description').innerHTML = content;
       document.querySelector('.contacts_modal').style.display = "";
     }
   })
   
   document.querySelector('.modal_footer button').addEventListener('click', (event) => {
-    subject = '';
-    description = '';
+    str = '';
+    content = '';
     document.querySelector('.subject').value = '';
     document.querySelector('.form_textarea').value = '';
     document.querySelector('.name').value = '';
